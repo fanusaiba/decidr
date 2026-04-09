4d863b4fc13cb12a11bd5512c6c4f089dfe547e5
Fanusaiba <nusaiba.fathima1703@gmail.com>
2026-04-10 02:59:19 +0530

    fix db

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.js";
 import decisionRoutes from "./routes/decisions.js";
 import checkinRoutes from "./routes/checkins.js";
 import analyticsRoutes from "./routes/analytics.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -42,6 +43,16 @@ app.use((err, req, res, next) => {
 });
 
 // start server
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Decidr server running on port ${PORT}`);
-});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB error:", err);
+  });
